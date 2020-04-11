154f5d5f5044288a904f74c4cc285ee03ab18809
afeiship <1290657123@qq.com>
2020-04-11 20:53:07 +0800

    chore: udpate scripts

--- dist/next-random.js
+++ dist/next-random.js
@@ -2,16 +2,16 @@
  * name: @feizheng/next-random
  * description: Create random number.
  * url: https://github.com/afeiship/next-random
- * version: 1.0.0
- * date: 2020-01-06 21:03:19
+ * version: 1.0.2
+ * date: 2020-04-11 20:52:55
  * license: MIT
  */
 
-(function() {
+(function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
 
-  nx.random = function(inMin, inMax) {
+  nx.random = function (inMin, inMax) {
     var addition = inMax - inMin;
     return parseInt(Math.random() * addition, 10) + inMin;
   };
--- src/next-random.js
+++ src/next-random.js
@@ -1,8 +1,8 @@
-(function() {
+(function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
 
-  nx.random = function(inMin, inMax) {
+  nx.random = function (inMin, inMax) {
     var addition = inMax - inMin;
     return parseInt(Math.random() * addition, 10) + inMin;
   };
